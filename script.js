1e4a98b961f8494d9d295f32f08faf1b596c188a
David Cho <gcho13@my.bcit.ca>
2023-05-19 01:43:26 -0700

    Functionalities implemented

---
+++ script.js
@@ -0,0 +1,225 @@
+async function getRandomPokemon(numPokemonPairs) {
+  const randomPokemon = new Set();
+  const shuffledPokemon = [];
+
+  while (randomPokemon.size < numPokemonPairs) {
+    const randomPokemonID = Math.floor(Math.random() * 810) + 1;
+    randomPokemon.add(randomPokemonID);
+    shuffledPokemon.push(randomPokemonID, randomPokemonID);
+  }
+  shuffledPokemon.sort(() => Math.random() - 0.5);
+
+  let cardsHtml = "";
+  for (let i = 0; i < shuffledPokemon.length; i++) {
+    const pokemonID = shuffledPokemon[i];
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`);
+    const data = await res.json();
+    cardsHtml += `
+            <div class="pokeCard">
+                <img id="img${i}" class="front_face" src=${data.sprites.other["official-artwork"].front_default} alt="">
+                <img class="back_face" src="back.webp" alt="">
+            </div>
+        `;
+  }
+  $("#gameGrid").append(cardsHtml);
+}
+
+const setup = () => {
+  $("#start").hide();
+  $("#reset").hide();
+
+  $("#reset").click(function () {
+    location.reload();
+  });
+
+  var darkModeText = $(".theme-switch-wrapper p").last();
+  var lightModeText = $(".theme-switch-wrapper p").first();
+  var darkModeImg = $("#img-dark");
+  var lightModeImg = $("#img-light");
+
+  darkModeImg.hide();
+  lightModeText.show();
+  darkModeText.hide();
+
+  $("#checkbox").change(function () {
+    if ($(this).is(":checked")) {
+      $("body").css("background-color", "black");
+      $(".dashboard").css("color", "white");
+      $(".dashboard").css("border", "1px solid white");
+
+      lightModeText.hide();
+      lightModeImg.hide();
+      darkModeText.show();
+      darkModeImg.show();
+    } else {
+      $("body").css("background-color", "white");
+      $(".dashboard").css("color", "black");
+      $(".dashboard").css("border", "1px solid black");
+
+      lightModeText.show();
+      lightModeImg.show();
+      darkModeText.hide();
+      darkModeImg.hide();
+    }
+  });
+
+  var difficulty = "";
+
+  $("#difficulty").change(function () {
+    difficulty = $(this).val();
+    if (difficulty) {
+      $("#start").show();
+      $("#reset").show();
+    }
+  });
+
+  function startGame() {
+    $("#start").css("display", "none");
+    $("#info").css("display", "");
+
+    var firstCard = undefined;
+    var secondCard = undefined;
+    var totalPairs = 0;
+    var matches = 0;
+    var clicks = 0;
+    var moves = 0;
+    var matchesLeft = totalPairs;
+    var timer = 0;
+    var finalTime = 0;
+
+    switch (difficulty) {
+      case "easy":
+        totalPairs = 3;
+        timer = 30;
+        $("#gameGrid").css("width", "300px");
+        $("#gameGrid").css("height", "200px");
+        break;
+      case "normal":
+        totalPairs = 6;
+        timer = 120;
+        $("#gameGrid").css("width", "400px");
+        $("#gameGrid").css("height", "300px");
+        break;
+      default:
+        totalPairs = 12;
+        timer = 180;
+        $("#gameGrid").css("width", "600px");
+        $("#gameGrid").css("height", "400px");
+    }
+    matchesLeft = totalPairs;
+
+    $("#moves").text(moves);
+    $("#total").text(totalPairs);
+    $("#matches").text(matches);
+    $("#left").text(matchesLeft - matches);
+    $("#timer").text(timer);
+    $("#time").text(timer);
+
+    let $clicks = $("#clicks");
+    let $moves = $("#moves");
+    let $matches = $("#matches");
+    let $left = $("#left");
+    let $finalMoves = $("#final-moves");
+    let $finalTime = $("#final-time");
+
+    let timerInterval = setInterval(() => {
+      timer--;
+      finalTime++;
+      $("#time").text(timer);
+
+      if (timer === 0) {
+        clearInterval(timerInterval);
+        $(".modal-timeout").show();
+        return;
+      }
+    }, 1000);
+
+    getRandomPokemon(totalPairs).then(() => {
+      $("#powerUp").one("click", function () {
+        $(this).prop("disabled", true);
+        $(".pokeCard").addClass("flip disabled");
+
+        setTimeout(() => {
+          $(".pokeCard").each(function () {
+            if (!$(this).hasClass("matched")) {
+              $(this).removeClass("flip disabled");
+            }
+          });
+        }, 1500);
+      });
+
+      $("#gameGrid").on("click", ".pokeCard", function () {
+        // Check for corner cases: if the card is already flipped, matched, or two cards are already flipped.
+        if (
+          $(this).hasClass("flip") ||
+          $(this).hasClass("matched") ||
+          (firstCard && secondCard)
+        ) {
+          return;
+        }
+
+        if (!firstCard) {
+          firstCard = $(this).find(".front_face")[0];
+          $(this).toggleClass("flip");
+          $(this).toggleClass("disabled");
+          clicks++;
+          $clicks.text(clicks);
+        } else {
+          if ($(this).find(".front_face")[0] === firstCard) {
+            return;
+          }
+          if (!secondCard) {
+            secondCard = $(this).find(".front_face")[0];
+            $(this).toggleClass("flip");
+            $(this).toggleClass("disabled");
+            clicks++;
+            $clicks.text(clicks);
+            moves++;
+            $moves.text(moves);
+          } else {
+            return;
+          }
+
+          if (firstCard.src == secondCard.src) {
+            matches++;
+            $matches.text(matches);
+            $left.text(matchesLeft - matches);
+
+            $(`#${firstCard.id}`).parent().addClass("matched");
+            $(`#${secondCard.id}`).parent().addClass("matched");
+
+            $(`#${firstCard.id}`).parent().off("click");
+            $(`#${secondCard.id}`).parent().off("click");
+
+            firstCard = undefined;
+            secondCard = undefined;
+          } else {
+            setTimeout(() => {
+              $(`#${firstCard.id}`).parent().toggleClass("flip");
+              $(`#${firstCard.id}`).parent().toggleClass("disabled");
+              $(`#${secondCard.id}`).parent().toggleClass("flip");
+              $(`#${secondCard.id}`).parent().toggleClass("disabled");
+
+              firstCard = undefined;
+              secondCard = undefined;
+            }, 1000);
+          }
+        }
+
+        if (matches === totalPairs) {
+          setTimeout(() => {
+            $finalMoves.text(moves);
+            $finalTime.text(finalTime);
+            $(".modal").show();
+            clearInterval(timerInterval);
+          }, 1000);
+        }
+      });
+    });
+  }
+  $("#start").click(function () {
+    startGame();
+  });
+};
+
+$(document).ready(setup);
